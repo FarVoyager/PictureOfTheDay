5d131f4f20143f6cc88ce4c1bae83d0d422b745b
Oleg <wowgameoleg@mail.ru>
2021-07-14 17:36:53 +0300

    animation Path Transition implemented

--- app/src/main/java/com/example/pictureoftheday/ui/main/animations/AnimationsFragment.kt
+++ app/src/main/java/com/example/pictureoftheday/ui/main/animations/AnimationsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
@@ -16,44 +17,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.pictureoftheday.R
 import com.example.pictureoftheday.databinding.FragmentAnimationsBinding
 import com.example.pictureoftheday.databinding.FragmentAnimationsEnlargeBinding
+import com.example.pictureoftheday.databinding.FragmentAnimationsPathTransitionsBinding
 
 class AnimationsFragment : Fragment() {
 
-    private var _binding: FragmentAnimationsEnlargeBinding? = null
+    private var _binding: FragmentAnimationsPathTransitionsBinding? = null
     private val binding get() = _binding!!
 
-    private var isExpanded = false
+    private var toRightAnimation = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentAnimationsEnlargeBinding.inflate(inflater, container, false)
+        _binding = FragmentAnimationsPathTransitionsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageView.setOnClickListener {
+        binding.button.setOnClickListener {
+            val changeBounds = ChangeBounds()
+            changeBounds.pathMotion = ArcMotion()
+            changeBounds.duration = 500
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer, changeBounds)
 
-            TransitionManager.beginDelayedTransition(
-                binding.container, TransitionSet()
-                    .addTransition(ChangeBounds())
-                    .addTransition(ChangeImageTransform())
-            )
+            toRightAnimation = !toRightAnimation
+            val params = binding.button.layoutParams as FrameLayout.LayoutParams
+            params.gravity = if (toRightAnimation) Gravity.END or Gravity.BOTTOM
+            else Gravity.START or Gravity.TOP
+            binding.button.layoutParams = params
 
-            val params: ViewGroup.LayoutParams = binding.imageView.layoutParams
-            params.height =
-                if (isExpanded) ViewGroup.LayoutParams.WRAP_CONTENT
-                else ViewGroup.LayoutParams.MATCH_PARENT
-            binding.imageView.layoutParams = params
-            binding.imageView.scaleType =
-                if (isExpanded) ImageView.ScaleType.FIT_CENTER
-                else ImageView.ScaleType.CENTER
 
-            isExpanded = !isExpanded
         }
     }
 }
